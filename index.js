309e806fcf69854beeddf78c8202861184d27a23
Curtis H <ch@curtish.me>
2015-10-11 12:51:18 +0100

    adding db inserts and updates

--- index.js
+++ index.js
@@ -7,13 +7,67 @@ var server  = app.listen(8002, function() {
     console.log('Dnd Soundboard listening on http://%s:%s', host, port);
 });
 
-var io      = require('socket.io')(server);
+var db;
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId    = require('mongodb').ObjectID;
 
+MongoClient.connect('mongodb://localhost:27017/soundboard', function(err, database) {
+    if(!err)
+        console.log("Connected correctly to server.");
+    
+    db = database;
+});
+
+
+
+var io      = require('socket.io')(server);
 io.on('connection', function(socket) {
+    
+    var cats = [];
+    var find = db.collection('categories').find();
+    
+    find.each(function(err, doc) {
+        if(doc != null) {
+           console.dir(doc);
+           cats.push(doc);
+        }
+        else if(err == null) {
+            console.log(cats);
+            socket.emit('categories', cats);
+        }
+     });
+    
+    
     socket.on('save', function(data) {
+        console.log('save');
         console.log(data);
-      
-        io.emit('saved', data);
+        
+        if(!data._id) {
+            data._id = ObjectId().toString();
+            
+            db.collection('categories').insertOne(data, function(err, result) {
+                console.log('result', err);
+                if(!err) {
+                    console.log('saved cat', result.ops[0]);
+                    io.emit('saved', result.ops[0]);
+                }
+            });
+        }
+        else {
+            var id = {'_id' : data._id};
+            delete(data._id);
+            
+            console.log('delete id');
+            console.log(data);
+            
+            db.collection('categories').updateOne(id,
+                {$set: data},
+                function(err, results) {
+                    console.log(results);
+                }
+            );
+            
+        }
     });
 });
 
--- public/main.js
+++ public/main.js
@@ -28,23 +28,47 @@ var app = angular.module('Soundboard', [])
             var id = getYoutubeId($scope.newSoundName);
             
             if(!!id && !!id[1]) {
+                
                 console.log(id);
                 id = id[1];
                 // TODO - check for existance
                 // TODO - save to server
+                $scope.focus.sounds.push(id);
+                console.log($scope.focus);
+                socket.emit('save', $scope.focus);
+                $scope.newSoundName = '';
                 
             }
-            return;
-            $scope.focus.sounds.push();
-            console.log($scope.focus);
-            $scope.newSoundName = '';
         }
     };
+    
+
+    socket.on('categories', function(cats) {
+        console.log('categories', cats);
+        $scope.$apply(function() {
+            console.log($scope.categories);
+            $scope.categories = cats;
+        });
+    });
     
     socket.on('saved', function(cat) {
         $scope.$apply(function() {
             console.log('saved', cat);
-            $scope.categories.push(cat);
+            var found = false;
+            
+            if($scope.categories.length) {
+                angular.forEach($scope.categories, function(category) {
+                    if(cat._id == category._id) {
+                        category = cat;
+                        found = true;
+                    }
+                });
+            }
+            
+            if(!found) {
+                $scope.categories.push(cat);
+            }
+            
             $scope.newCategoryName = '';
         });
     });
@@ -63,9 +87,9 @@ app.directive('youtube', function() {
         },
         template: '<div><h3>Sound {{ $id }}</h3><div class="controls"><button ng-click="toggle()">Play</button></div><div class="player"></div></div>',
         link: function($scope, element, attrs) {
-            console.log($scope.data);
+            var videoId = $scope.data;
+            var playing = false
             var player;
-            var playing = false;
             
             $scope.ready = false;
             
@@ -83,12 +107,11 @@ app.directive('youtube', function() {
             
             function onYouTubeIframeAPIReady() {
                 var elem = element[0].querySelector('.player');
-                var vid  = getVideoId($scope.data);
                 
                 player = new YT.Player(elem, {
                     height: '0',
                     width: '0',
-                    videoId: vid,
+                    videoId: videoId,
                     events: {
                       'onReady': onPlayerReady,
                       'onStateChange': onPlayerStateChange
